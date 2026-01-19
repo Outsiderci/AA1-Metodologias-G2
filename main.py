67ba5d505e31c2f1aac5dabe6bcea74a00cffc4d
Kevin Alexander Chusin Acaro <kachusin@espe.edu.ec>
2026-01-18 21:12:16 -0500

    Agregado menu de consola main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from usuario import Usuario
+
+def mostrar_menu():
+    print("\n--- SISTEMA DE GESTIÓN DE USUARIOS ---")
+    print("1. Registrar nuevo usuario")
+    print("2. Salir")
+    
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+        
+        if opcion == '1':
+            print("\n--- REGISTRO ---")
+            nombre = input("Ingrese nombre: ")
+            email = input("Ingrese email: ")
+            clave = input("Ingrese contraseña: ")
+            
+            # Llamamos a la lógica que ya creaste
+            resultado = Usuario.registrar(nombre, email, clave)
+            print(f">> RESULTADO: {resultado}")
+            
+        elif opcion == '2':
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    main()
